a3b39436b0dc10035d4271879e03a5a4c660e6ea
Fernando <Fernando@DESKTOP-GMC23D7>
2022-05-17 19:57:23 -0500

    Primera edición

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module taller1_repo_lm_1 {
+}
---
+++ src/taller1_repo_lm/Main.java
@@ -0,0 +1,12 @@
+package taller1_repo_lm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Mi primer proyecto JAVa en GitHub");
+		
+	}
+
+}
